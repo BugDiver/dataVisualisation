2c90ca20827b73a8373f06cd961322ab18424716
BugDiver <vinaysh@thoughtworks.com>
2016-11-12 16:55:50 +0530

    refactoring

--- js/exercise5.js
+++ js/exercise5.js
@@ -1,6 +1,5 @@
-var data = [0,1,2,3,4,5,6,7,8,9,10];
-
 var loadChart = function () {
+    var data = [0,1,2,3,4,5,6,7,8,9,10];
 
     var fontScale = d3.scaleLinear().domain([0,10]).range(["italic bold 12px/30px","italic bold 120px/180px serif"]);
     var widthScale = d3.scaleQuantize().domain([9,10]).range([60,120]);
