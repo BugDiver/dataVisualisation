9e01562a64f3777dfd76c1216abe598dfaaf4527
BugDiver <vinaysh@thoughtworks.com>
2016-11-15 16:12:30 +0530

    refactoring.

--- js/exercise6.js
+++ js/exercise6.js
@@ -1,52 +1,28 @@
 var showShapes = function () {
     var height = 100;
     var width = 600;
-    var starting = 0;
     var shapeWidth = 100;
     var shapeMargin = 50;
 
-    var separator = ",";
-    var space = " ";
+    var shapes = [{name: 'line', x1: 100, y1: 0, x2: 0, y2: 100,'stroke':'grey'},
+        {name: 'circle', cx: 50, cy: 50, r: 50,'stroke':'red'},
+        {name: 'rect', x: 0, y: 0, r: 50, width: 100, height: 100, rx: 5, ry: 5,'stroke':"steelblue"},
+        {name: 'polygon', points: '50,0 0,100 100,100','stroke':'yellow'}];
 
+    var translate = function (x, y) {
+        return 'translate(' + x + ',' + (y || 0) + ')';
+    };
+    
     var svg = d3.select(".container").append("svg")
         .attr("width", width)
         .attr("height", height);
 
-    svg.append("line")
-        .attr("x1", shapeWidth)
-        .attr("y1", starting)
-        .attr("x2", starting)
-        .attr("y2", shapeWidth)
-        .attr("stroke-width", 4)
-        .attr("stroke", "grey");
-
-    svg.append("circle")
-        .attr("cx", shapeWidth + shapeMargin + shapeWidth / 2)
-        .attr("cy", shapeWidth / 2)
-        .attr("r", shapeWidth / 2)
-        .attr("stroke", "red")
-        .attr("stroke-width", "3")
-        .attr("fill", "none");
-
-    svg.append("rect")
-        .attr("x", shapeWidth * 2 + shapeMargin * 2)
-        .attr("y", starting)
-        .attr("width", shapeWidth)
-        .attr("height", shapeWidth)
-        .style("stroke-width", "3")
-        .style("fill", "none")
-        .attr("stroke", "skyblue")
-        .attr("rx", "5px")
-        .attr("ry", "5px");
-    
-    svg.append("polygon")
-        .attr("stroke", "green")
-        .attr("stroke-width", "3")
-        .attr("fill", "none")
-        .attr("points", (shapeWidth * 3 + shapeMargin * 3) + separator + height + separator + space +
-                        (shapeWidth * 3 + shapeMargin * 4) + separator + starting + separator + space +
-                        (shapeWidth * 4 + shapeMargin * 3) + separator + height);
-
+    shapes.forEach(function (s, i) {
+        var g = svg.append("g").attr("transform", translate(i * (shapeWidth + shapeMargin)));
+        var shape = g.append(s.name).classed('shapes',true);
+        delete s.name;
+        Object.keys(s).forEach(function(a){shape.attr(a,s[a]);})
+    });
 };
 
 window.onload = showShapes();
